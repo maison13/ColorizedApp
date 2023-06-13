fdfeef52398c6fd61b39c222c5357b808d3b4c9d
Vladimir <smile_friend@mail.ru>
2023-06-14 01:33:55 +0300

    setup all logic

--- ColorizedApp/ContentView.swift
+++ ColorizedApp/ContentView.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    private enum Field {
+        case red
+        case green
+        case blue
+    }
+    
     @State private var sliderRedValue = Double.random(in: 0...255)
     @State private var sliderGreenValue = Double.random(in: 0...255)
     @State private var sliderBlueValue = Double.random(in: 0...255)
-    
-    @State private var textRedSlider = ""
-    @State private var textGreenSlider = ""
-    @State private var textBlueSlider = ""
-    
-    var body: some View {	
+
+    @FocusState private var focusedField: Field?
+
+    var body: some View {
         
         ZStack {
-            Color(hue: 0.6, saturation: 1, brightness: 0.75).ignoresSafeArea()
-           
+            Color(hue: 0.6, saturation: 1, brightness: 0.75)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    focusedField = nil
+                }
             VStack {
                 RoundedRectangle(cornerRadius: 15)
                     .foregroundColor(Color(red: sliderRedValue/255, green: sliderGreenValue/255, blue: sliderBlueValue/255))
@@ -29,18 +36,20 @@ struct ContentView: View {
                 
                 Spacer().frame(height: 50)
                 
-                ColorSliderView(value: $sliderRedValue, textValue: $textRedSlider, sliderColor: .red)
-                ColorSliderView(value: $sliderGreenValue, textValue: $textGreenSlider, sliderColor: .green)
-                ColorSliderView(value: $sliderBlueValue, textValue: $textBlueSlider, sliderColor: .blue)
-                
+                ColorSliderView(sliderValue: $sliderRedValue, sliderColor: .red)
+                    .focused($focusedField, equals: .red)
+                ColorSliderView(sliderValue: $sliderGreenValue, sliderColor: .green)
+                    .focused($focusedField, equals: .green)
+                ColorSliderView(sliderValue: $sliderBlueValue, sliderColor: .blue)
+                    .focused($focusedField, equals: .blue)
+                    
                 Spacer()
             }
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
                     Spacer()
-                    
                     Button("Done") {
-                        
+                        focusedField = nil
                     }
                 }
             }
@@ -48,55 +57,60 @@ struct ContentView: View {
         }
     }
 }
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
+// MARK: - Setup Sliders & TextField
 struct ColorSliderView: View {
-    @Binding var value: Double
-    @Binding var textValue: String
+    @Binding var sliderValue: Double
+    @State var textValue = ""
     @State private var isPresented = false
     
     let sliderColor: Color
     
     var body: some View {
         HStack {
-            Text("\(lround(value))")
+            Text("\(lround(sliderValue))")
                 .font(.system(size: 16))
                 .foregroundColor(.white)
                 .frame(width: 40)
                 .multilineTextAlignment(.leading)
             
-            Slider(value: $value, in: 0...255, step: 1)
+            Slider(value: $sliderValue, in: 0...255, step: 1)
                 .accentColor(sliderColor)
+                .onChange(of: sliderValue) {
+                    textValue = sliderValue.formatted()
+                }
             
             Spacer()
-            TextField("\(lround(value))", text: $textValue)
+            
+            TextField("", text: $textValue) { _ in
+                withAnimation { checkValue() }
+            }
                 .font(.system(size: 16))
                 .background()
                 .cornerRadius(5)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .textFieldStyle(.roundedBorder)
                 .frame(width: 50)
                 .multilineTextAlignment(.trailing)
                 .keyboardType(.numberPad)
-                
                 .alert("Неправильное значение", isPresented: $isPresented, actions: {}) {
-                    Text("Введите значение от 0 до 255")
+                Text("Введите значение от 0 до 255")
                 }
-                
         }
+        .onAppear {textValue = "\(lround(sliderValue))"}
     }
+   
     private func checkValue() {
-        if value > 255 {
-            isPresented.toggle()
+        if let value = Double(textValue), (0...255).contains(value) {
+            sliderValue = value
+            return
         }
-        if value < 0 {
-            isPresented.toggle()
-        }
+        isPresented.toggle()
+        sliderValue = 0.0
+        textValue = "0"
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
